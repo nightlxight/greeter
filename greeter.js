3099fa62a72bb56ba391c3fdde4a1f74fc58da1c
nightlxight <itsbeadj@gmail.com>
2021-09-18 14:32:00 +0300

    Update greeter.js

--- greeter.js
+++ greeter.js
@@ -2,6 +2,11 @@ var fs = require('fs');
 const yaml = require('js-yaml');
 const config = yaml.load(fs.readFileSync('./configuration.yml'));
 
+
+function day() {
+    
+}
+
 if (new Date().getHours() < 18) {
     console.log("Good day!");
 } else {
